fa06e5cc660e1f8f2adf9251f85b1ad4e32beb38
Desislava <desislava2810@abv.bg>
2022-03-25 12:52:59 +0200

    refactor: Change script check logic

--- scripts/prepare-env-dev.sh
+++ scripts/prepare-env-dev.sh
@@ -2,22 +2,22 @@
 set -euxo pipefail
 
 if [[ $# -eq 0 ]]; then
-  if [[ ${CI_JOB_TOKEN+} ]]; then
+ # if [[ -z ${CI_JOB_TOKEN} ]]; then
+    #echo "Error: there is no PRIVATE or CI_JOB token"
+    #exit 1
+  #else
     TOKEN_TYPE="JOB-TOKEN"
-    TOKEN_VALUE=${CI_JOB_TOKEN}
-  else
-    echo "Error: there is no PRIVATE or CI_JOB token"
-    exit 1
-  fi
+    TOKEN_VALUE="$CI_JOB_TOKEN"
+  #fi
 else
   TOKEN_TYPE="PRIVATE-TOKEN"
-  TOKEN_VALUE=$1
+  TOKEN_VALUE="$1"
 fi
 
 ROOT_DIR=$(pwd)
 
 VERSION=$(curl --silent "https://net-dev.nolus.io:26612/abci_info" | jq '.result.response.version' | tr -d '"')
-curl --output artifacts.zip --header "${TOKEN_TYPE}: ${TOKEN_VALUE}" "https://gitlab-nomo.credissimo.net/api/v4/projects/3/jobs/artifacts/v${VERSION}/download?job=setup-dev-network"
+curl --output artifacts.zip --header "$TOKEN_TYPE: $TOKEN_VALUE" "https://gitlab-nomo.credissimo.net/api/v4/projects/3/jobs/artifacts/v$VERSION/download?job=setup-dev-network"
 echo 'A' | unzip artifacts.zip
 
 USER_1_ACCOUNTS_DIR="$ROOT_DIR/accounts"
