da2ef0414bb91ca86c6b59262bb053ebc332e43f
Desislava <d.paunova@nolus.io>
2022-04-21 10:54:51 +0300

    fix: Fix token types logic

--- scripts/prepare-env-dev.sh
+++ scripts/prepare-env-dev.sh
@@ -9,6 +9,8 @@ IBC_TOKEN='ibc/0954E1C28EB7AF5B72D24F3BC2B47BBB2FDF91BDDFD57B74B99E133AED40972A'
 SMART_CONTRACTS_PROJECT_ID="8"
 COSMZONE_PROJECT_ID="3"
 CONTRACTS_INFO_ARTIFACT="deploy:cargo"
+SETUP_DEV_NETWORK_ARTIFACT="setup-dev-network"
+NOLUS_BUILD_BINARY_ARTIFACT="build-binary"
 
 if [[ $# -eq 0 ]]; then
  if [[ -z ${CI_JOB_TOKEN+x} ]]; then
@@ -17,9 +19,11 @@ if [[ $# -eq 0 ]]; then
   fi
     TOKEN_TYPE="JOB-TOKEN"
     TOKEN_VALUE="$CI_JOB_TOKEN"
+    TAGS_ENDPOINT_ACCESS_TOKEN="$SMART_CONTRACTS_ACCESS_KEY"
 else
   TOKEN_TYPE="PRIVATE-TOKEN"
   TOKEN_VALUE="$1"
+  TAGS_ENDPOINT_ACCESS_TOKEN="$1"
 fi
 
 downloadArtifact() {
@@ -27,7 +31,7 @@ downloadArtifact() {
   local version="$2"
   local project_id="$3"
 
-  curl --output "$name".zip --header "$TOKEN_TYPE: $TOKEN_VALUE" "$GITLAB_API/projects/$project_id/jobs/artifacts/v$version/download?job=$name"
+  curl --output "$name".zip --header "$TOKEN_TYPE: $TOKEN_VALUE" "$GITLAB_API/projects/$project_id/jobs/artifacts/$version/download?job=$name"
   echo 'A' | unzip "$name".zip
 }
 
@@ -47,8 +51,8 @@ exportKey() {
 
 COSMZONE_LATEST_VERSION=$(curl --silent "$NOLUS_DEV_NET/abci_info" | jq '.result.response.version' | tr -d '"')
 
-downloadArtifact "setup-dev-network" "$COSMZONE_LATEST_VERSION" "$COSMZONE_PROJECT_ID"
-downloadArtifact "build-binary" "$COSMZONE_LATEST_VERSION" "$COSMZONE_PROJECT_ID"
+downloadArtifact "$SETUP_DEV_NETWORK_ARTIFACT" "v$COSMZONE_LATEST_VERSION" "$COSMZONE_PROJECT_ID"
+downloadArtifact "$NOLUS_BUILD_BINARY_ARTIFACT" "v$COSMZONE_LATEST_VERSION" "$COSMZONE_PROJECT_ID"
 
 tar -xf $ARTIFACT_BIN
 export PATH
@@ -65,7 +69,7 @@ USER_3_PRIV_KEY=$(exportKey "test-user-2")
 # Get contracts information
 # download deploy:cargo artifact from smart-contracts
 
-SMART_CONTRACTS_LATEST_VERSION=$(curl --header "PRIVATE-TOKEN: $SMART_CONTRACTS_ACCESS_KEY" "$GITLAB_API/projects/$SMART_CONTRACTS_PROJECT_ID/repository/tags" | jq -r '.[0].name' | tr -d '"')
+SMART_CONTRACTS_LATEST_VERSION=$(curl --header "PRIVATE-TOKEN: $TAGS_ENDPOINT_ACCESS_TOKEN" "$GITLAB_API/projects/$SMART_CONTRACTS_PROJECT_ID/repository/tags" | jq -r '.[0].name' | tr -d '"')
 downloadArtifact  "$CONTRACTS_INFO_ARTIFACT" "$SMART_CONTRACTS_LATEST_VERSION" "$SMART_CONTRACTS_PROJECT_ID"
 
 # download the schemas
