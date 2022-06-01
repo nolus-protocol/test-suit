523864f9139a19f399c2b6cfd7ba38f15fbbaa42
Rusi Boyadjiev <rusiboyadjiev@gmail.com>
2022-06-01 14:44:14 +0300

    refactoring: clients
    Signed-off-by: Rusi Boyadjiev <rusiboyadjiev@gmail.com>

--- src/util/clients.ts
+++ src/util/clients.ts
@@ -1,32 +1,7 @@
-import {
-  SigningCosmWasmClient,
-  SigningCosmWasmClientOptions,
-} from '@cosmjs/cosmwasm-stargate';
-import { DirectSecp256k1Wallet, Registry } from '@cosmjs/proto-signing';
+import { makeCosmoshubPath } from '@cosmjs/amino';
+import { DirectSecp256k1Wallet } from '@cosmjs/proto-signing';
 import { fromHex } from '@cosmjs/encoding';
-import {
-  MsgClearAdmin,
-  MsgExecuteContract,
-  MsgInstantiateContract,
-  MsgMigrateContract,
-  MsgStoreCode,
-  MsgUpdateAdmin,
-} from 'cosmjs-types/cosmwasm/wasm/v1/tx';
-import { generateMnemonic, mnemonicToSeedSync } from 'bip39';
-import { fromSeed } from 'bip32';
-import { defaultRegistryTypes } from '@cosmjs/stargate';
-import {
-  MsgCreateVestingAccount,
-  protobufPackage as vestingPackage,
-} from './codec/cosmos/vesting/v1beta1/tx';
-import {
-  MsgSuspend,
-  MsgUnsuspend,
-  protobufPackage as suspendPackage,
-} from './codec/nolus/suspend/v1beta1/tx';
-import { QuerySuspendRequest } from './codec/nolus/suspend/v1beta1/query';
-import { NOLUS_PREFIX } from '../util/utils';
-import { ChainConstants, NolusWallet } from '@nolus/nolusjs';
+import { ChainConstants, KeyUtils, NolusWallet } from '@nolus/nolusjs';
 import { nolusOfflineSigner } from '@nolus/nolusjs/build/wallet/NolusWalletFactory';
 
 const user1PrivKey = fromHex(process.env.USER_1_PRIV_KEY as string);
@@ -46,23 +21,6 @@ export async function getWallet(privateKey: Uint8Array): Promise<NolusWallet> {
 
 export default NODE_ENDPOINT;
 
-// export async function getClientWithKey(
-//   privateKey: Uint8Array,
-// ): Promise<SigningCosmWasmClient> {
-//   const wallet = await getWallet(privateKey);
-//   return getClient(wallet.);
-// }
-
-// export async function getClient(
-//   wallet: DirectSecp256k1Wallet,
-// ): Promise<SigningCosmWasmClient> {
-//   return await SigningCosmWasmClient.connectWithSigner(
-//     NODE_ENDPOINT,
-//     wallet,
-//     getSignerOptions(),
-//   );
-// }
-
 export function getValidatorAddress(): string {
   return process.env.VALIDATOR_1_ADDRESS as string;
 }
@@ -79,50 +37,10 @@ export async function getUser3Wallet(): Promise<NolusWallet> {
   return await getWallet(user3PrivKey);
 }
 
-// export async function getUser1Client(): Promise<SigningCosmWasmClient> {
-//   return await getClientWithKey(user1PrivKey);
-//   const wallet = await getWallet(user1PrivKey);
-// }
-//
-// export async function getUser2Client(): Promise<SigningCosmWasmClient> {
-//   return await getClientWithKey(user2PrivKey);
-// }
-//
-// export async function getUser3Client(): Promise<SigningCosmWasmClient> {
-//   return await getClientWithKey(user3PrivKey);
-// }
-
-export async function createWallet(): Promise<DirectSecp256k1Wallet> {
-  const privateKey = seedToPrivateKey(generateMnemonic(256));
-  return await DirectSecp256k1Wallet.fromKey(privateKey, NOLUS_PREFIX);
-}
-
-function seedToPrivateKey(
-  mnemonic: string,
-  hdPath = "m/44'/118'/0'/0/0",
-): Buffer {
-  const seed = mnemonicToSeedSync(mnemonic);
-  const masterKey = fromSeed(seed);
-  const { privateKey } = masterKey.derivePath(hdPath);
-  if (privateKey === undefined) {
-    throw new Error('Illegal state reached');
-  }
-  return privateKey;
-}
-
-function getSignerOptions(): SigningCosmWasmClientOptions {
-  const customRegistry = new Registry([
-    ...defaultRegistryTypes,
-    ['/cosmwasm.wasm.v1.MsgClearAdmin', MsgClearAdmin],
-    ['/cosmwasm.wasm.v1.MsgExecuteContract', MsgExecuteContract],
-    ['/cosmwasm.wasm.v1.MsgMigrateContract', MsgMigrateContract],
-    ['/cosmwasm.wasm.v1.MsgStoreCode', MsgStoreCode],
-    ['/cosmwasm.wasm.v1.MsgInstantiateContract', MsgInstantiateContract],
-    ['/cosmwasm.wasm.v1.MsgUpdateAdmin', MsgUpdateAdmin],
-    [`/${vestingPackage}.MsgCreateVestingAccount`, MsgCreateVestingAccount],
-    [`/${suspendPackage}.MsgSuspend`, MsgSuspend],
-    [`/${suspendPackage}.MsgUnsuspend`, MsgUnsuspend],
-    [`/${suspendPackage}.QuerySuspendRequest`, QuerySuspendRequest],
-  ]);
-  return { registry: customRegistry };
+export async function createWallet(): Promise<NolusWallet> {
+  const mnemonic = KeyUtils.generateMnemonic();
+  const accountNumbers = [0];
+  const path = accountNumbers.map(makeCosmoshubPath)[0];
+  const privateKey = await KeyUtils.getPrivateKeyFromMnemonic(mnemonic, path);
+  return getWallet(privateKey);
 }
