747bb728e5495e0e9b2385228bc10b966a02c0d4
Desislava <d.paunova@nolus.io>
2022-06-09 15:08:33 +0300

    fix: Remove invalid scenarios

--- src/leaser/leaseConfig.test.ts
+++ src/leaser/leaseConfig.test.ts
@@ -64,34 +64,6 @@ describe('Leaser contract tests - Config', () => {
     await expect(result).rejects.toThrow(/^.*Unauthorized.*/);
   });
 
-  test('the business tries to set the initial liability to 0% - should produce an error', async () => {
-    leaserConfigMsg.config.liability.initial = 0;
-
-    const result = () =>
-      leaseInstance.setLeaserConfig(
-        leaserContractAddress,
-        user1Wallet,
-        leaserConfigMsg,
-        DEFAULT_FEE,
-      );
-
-    await expect(result).rejects.toThrow(/^.*failed to execute message.*/);
-  });
-
-  test('the business tries to set the repayment period to 0 sec - should produce an error', async () => {
-    leaserConfigMsg.config.repayment.period_sec = 0;
-
-    const result = () =>
-      leaseInstance.setLeaserConfig(
-        leaserContractAddress,
-        user1Wallet,
-        leaserConfigMsg,
-        DEFAULT_FEE,
-      );
-
-    await expect(result).rejects.toThrow(/^.*failed to execute message.*/);
-  });
-
   test('the business tries to set initial liability % > healthy liability % - should produce an error', async () => {
     leaserConfigMsg.config.liability.initial =
       leaserConfigMsg.config.liability.healthy + 1;
