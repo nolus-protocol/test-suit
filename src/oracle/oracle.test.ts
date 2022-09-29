3590c0a281abd2e273b3503a5b70baea4edc9885
Desislava <d.paunova@nolus.io>
2022-09-29 18:36:34 +0300

    feat: Oracle more tests

--- src/oracle/oracle.test.ts
+++ src/oracle/oracle.test.ts
@@ -11,22 +11,25 @@ import NODE_ENDPOINT, {
 } from '../util/clients';
 import { NolusClient, NolusContracts, NolusWallet } from '@nolus/nolusjs';
 import { sendInitExecuteFeeTokens } from '../util/transfer';
+import { FeedPrices } from '@nolus/nolusjs/build/contracts';
 
 describe('Oracle contract tests', () => {
-  let amountFeederWallet: NolusWallet;
+  let wasmAdminWallet: NolusWallet;
+  let userWithBalance: NolusWallet;
   let feederWallet: NolusWallet;
   let oracleInstance: NolusContracts.Oracle;
   let PRICE_FEED_PERIOD: number;
-  let PERCENTAGE_NEEDED: number;
+  let PERMILLE_NEEDED: number;
   let BASE_ASSET: string;
   let supportedPairsBefore: string[][];
+  let feedPrices: FeedPrices;
   const testPairMember = 'UAT';
   const oracleContractAddress = process.env.ORACLE_ADDRESS as string;
 
   beforeAll(async () => {
     NolusClient.setInstance(NODE_ENDPOINT);
-    amountFeederWallet = await getWasmAdminWallet();
-    const userWithBalance = await getUser1Wallet();
+    wasmAdminWallet = await getWasmAdminWallet();
+    userWithBalance = await getUser1Wallet();
     feederWallet = await createWallet();
 
     const cosm = await NolusClient.getInstance().getCosmWasmClient();
@@ -36,8 +39,8 @@ describe('Oracle contract tests', () => {
 
     BASE_ASSET = config.base_asset;
 
-    PRICE_FEED_PERIOD = config.price_feed_period_secs;
-    PERCENTAGE_NEEDED = config.feeders_percentage_needed;
+    PRICE_FEED_PERIOD = config.price_feed_period / 1000000000; //nanosec to sec
+    PERMILLE_NEEDED = config.expected_feeders;
 
     const adminBalance = {
       amount: '10000000',
@@ -45,14 +48,14 @@ describe('Oracle contract tests', () => {
     };
 
     await userWithBalance.transferAmount(
-      amountFeederWallet.address as string,
+      wasmAdminWallet.address as string,
       [adminBalance],
       customFees.transfer,
     );
 
     // send some tokens to the feeder
     await sendInitExecuteFeeTokens(
-      amountFeederWallet,
+      wasmAdminWallet,
       feederWallet.address as string,
     );
 
@@ -65,7 +68,7 @@ describe('Oracle contract tests', () => {
     expect(isFeeder).toBe(false);
 
     await oracleInstance.addFeeder(
-      amountFeederWallet,
+      wasmAdminWallet,
       feederWallet.address as string,
       customFees.exec,
     );
@@ -76,37 +79,25 @@ describe('Oracle contract tests', () => {
     newSupportedPairs.push([testPairMember, BASE_ASSET]);
 
     await oracleInstance.updateSupportPairs(
-      amountFeederWallet,
+      wasmAdminWallet,
       newSupportedPairs,
       customFees.exec,
     );
   });
 
   test('the feeder should be added', async () => {
-    // query - is feeder
-
     const isFeeder = await oracleInstance.isFeeder(
       feederWallet.address as string,
     );
     expect(isFeeder).toBe(true);
   });
 
-  // test('push new price feed for unsupported denom pairs - should produce an error', async () => {
-  // TO DO
-  // });
-
-  // test('feed nested price should works as expected', async () => {  // [OSMO,UST] [B,UST] -> get OSMO,B
-  // TO DO
-  // });
-
-  // TO DO: Alarm ?
-
-  test('feed price should works as expected', async () => {
+  test('feed price - should work as expected', async () => {
     // change percentage needed to 1%
     await oracleInstance.setConfig(
-      amountFeederWallet,
+      wasmAdminWallet,
       PRICE_FEED_PERIOD,
-      1,
+      10, //permille
       customFees.exec,
     );
 
@@ -114,6 +105,7 @@ describe('Oracle contract tests', () => {
 
     // calc needed votes
     const onePercentNeeded = Math.trunc(listFeeders.length / 100) + 1; // 1%
+    console.log(listFeeders.length, onePercentNeeded);
 
     // create the required number of feeders - 1
     for (let i = 1; i < onePercentNeeded; i++) {
@@ -121,23 +113,23 @@ describe('Oracle contract tests', () => {
       const newFeederWallet = await createWallet();
 
       await oracleInstance.addFeeder(
-        amountFeederWallet,
+        wasmAdminWallet,
         newFeederWallet.address as string,
         customFees.exec,
       );
 
       // send tokens to the new feeder
       await sendInitExecuteFeeTokens(
-        amountFeederWallet,
+        wasmAdminWallet,
         newFeederWallet.address as string,
       );
 
       // feed price
-      const feedPrices = {
+      feedPrices = {
         prices: [
           {
-            base: { amount: '11', symbol: testPairMember },
-            quote: { amount: '1', symbol: BASE_ASSET },
+            amount: { amount: '11', symbol: testPairMember }, // any amount
+            amount_quote: { amount: '1', symbol: BASE_ASSET }, // any amount
           },
         ],
       };
@@ -149,33 +141,33 @@ describe('Oracle contract tests', () => {
       );
     }
 
-    const price = () => oracleInstance.getPricesFor([testPairMember]);
+    const price = () => oracleInstance.getPriceFor(testPairMember);
 
     // not enough votes yet
-    await expect(price).rejects.toThrow(/^.*No price for pair.*/);
+    await expect(price).rejects.toThrow(/^.*No price.*/);
 
     // create the last required feeder
     const lastFeederWallet = await createWallet();
 
     await oracleInstance.addFeeder(
-      amountFeederWallet,
+      wasmAdminWallet,
       lastFeederWallet.address as string,
       customFees.exec,
     );
 
-    await amountFeederWallet.transferAmount(
+    await wasmAdminWallet.transferAmount(
       lastFeederWallet.address as string,
       customFees.exec.amount,
       customFees.transfer,
     );
 
-    const EXPECTED_PRICE = '3';
-
-    const feedPrices = {
+    const EXPECTED_AMOUNT_QUOTE = '3';
+    const EXPECTED_AMOUNT = '10';
+    feedPrices = {
       prices: [
         {
-          base: { amount: '10', symbol: testPairMember },
-          quote: { amount: EXPECTED_PRICE, symbol: BASE_ASSET },
+          amount: { amount: EXPECTED_AMOUNT, symbol: testPairMember }, // any amount
+          amount_quote: { amount: EXPECTED_AMOUNT_QUOTE, symbol: BASE_ASSET },
         },
       ],
     };
@@ -186,31 +178,139 @@ describe('Oracle contract tests', () => {
       customFees.exec,
     );
 
-    const afterResult = await oracleInstance.getPricesFor([testPairMember]);
+    const afterResult = await oracleInstance.getPriceFor(testPairMember);
 
     // already enough votes - the price should be the last added value
-    expect(afterResult.prices[0].quote.amount).toBe(EXPECTED_PRICE);
-    expect(afterResult.prices[0].quote.symbol).toBe(BASE_ASSET);
+    expect(afterResult.amount_quote.amount).toBe(EXPECTED_AMOUNT_QUOTE);
+    expect(afterResult.amount_quote.symbol).toBe(BASE_ASSET);
+    expect(afterResult.amount.amount).toBe(EXPECTED_AMOUNT);
+    expect(afterResult.amount.symbol).toBe(testPairMember);
 
     // the price feed period has expired + block creation time
     await sleep(BLOCK_CREATION_TIME_DEV_SEC + PRICE_FEED_PERIOD);
     const resultAfterPeriod = () =>
       oracleInstance.getPricesFor([testPairMember]);
-    await expect(resultAfterPeriod).rejects.toThrow(/^.*No price for pair.*/);
+    await expect(resultAfterPeriod).rejects.toThrow(/^.*No price.*/);
 
     // set config to the init state
     await oracleInstance.setConfig(
-      amountFeederWallet,
+      wasmAdminWallet,
       PRICE_FEED_PERIOD,
-      PERCENTAGE_NEEDED,
+      PERMILLE_NEEDED,
       customFees.exec,
     );
 
-    // set SupportPairs to the init state
+    // set supported pairs to the init state
     await oracleInstance.updateSupportPairs(
-      amountFeederWallet,
+      wasmAdminWallet,
       supportedPairsBefore,
       customFees.exec,
+    );
+  });
+
+  // test('feed nested price should works as expected', async () => {  // [OSMO,UST] [B,UST] -> get OSMO,B
+  // TO DO
+  // });
+
+  // TO DO: Alarm ?
+
+  test('only the wasm admin should be able to change the config', async () => {
+    const result = () =>
+      oracleInstance.setConfig(userWithBalance, 1, 1, customFees.exec); // any feederPercentage and pricePeriod
+
+    await expect(result).rejects.toThrow('Unauthorized');
+  });
+
+  test('only the wasm admin should be able to change supported pairs', async () => {
+    const result = () =>
+      oracleInstance.updateSupportPairs(
+        userWithBalance,
+        supportedPairsBefore,
+        customFees.exec,
+      );
+
+    await expect(result).rejects.toThrow('Unauthorized');
+  });
+
+  test('only the wasm admin should be able to add feeder', async () => {
+    const result = () =>
+      oracleInstance.addFeeder(
+        userWithBalance,
+        userWithBalance.address as string,
+        customFees.exec,
+      );
+
+    await expect(result).rejects.toThrow('Unauthorized');
+  });
+
+  test('only the wasm admin should be able to remove feeder', async () => {
+    const result = () =>
+      oracleInstance.removeFeeder(
+        userWithBalance,
+        feederWallet.address as string,
+        customFees.exec,
+      );
+
+    await expect(result).rejects.toThrow('Unauthorized');
+  });
+
+  test('only a registered feeder should be able to push prices', async () => {
+    const result = () =>
+      oracleInstance.feedPrices(userWithBalance, feedPrices, customFees.exec);
+
+    await expect(result).rejects.toThrow(
+      'No feeder data for the specified address',
+    );
+  });
+
+  test('the wasm admin tries to add an invalid feeder address', async () => {
+    const invalidAddress = 'nolus1ta43kkqwmugfdrddvdy4ewcgyw2n9maaaaaaaa';
+    const result = () =>
+      oracleInstance.addFeeder(
+        wasmAdminWallet,
+        invalidAddress,
+        customFees.exec,
+      );
+
+    await expect(result).rejects.toThrow('invalid checksum');
+  });
+
+  test('the wasm admin tries to remove a non-existent feeder', async () => {
+    const newWallet = await createWallet();
+
+    const result = () =>
+      oracleInstance.removeFeeder(
+        wasmAdminWallet,
+        newWallet.address as string,
+        customFees.exec,
+      );
+
+    await expect(result).rejects.toThrow(
+      'No feeder data for the specified address',
+    );
+  });
+
+  test('the wasm admin tries to set an invalid config - should produce an error', async () => {
+    // price feed period = 0
+    const result1 = () =>
+      oracleInstance.setConfig(wasmAdminWallet, 0, 1, customFees.exec); // any precentage needed
+
+    await expect(result1).rejects.toThrow('Price feed period can not be 0');
+
+    // feeder precentage needed = 0
+    const result2 = () =>
+      oracleInstance.setConfig(wasmAdminWallet, 1, 0, customFees.exec); // any pricePeriod
+
+    await expect(result2).rejects.toThrow(
+      'Percent of expected available feeders should be > 0 and <= 100',
+    );
+
+    // feeder precentage needed > 100%, 1000permille
+    const result3 = () =>
+      oracleInstance.setConfig(wasmAdminWallet, 1, 1001, customFees.exec); // any pricePeriod
+
+    await expect(result3).rejects.toThrow(
+      'Percent of expected available feeders should be > 0 and <= 100',
     );
   });
 });
