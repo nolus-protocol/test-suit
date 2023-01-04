b6ad79279453b86afda44b249524fc149549ade8
Desislava <d.paunova@nolus.io>
2023-01-04 11:50:30 +0200

    update: Treasury - a dispatcher can only be contract

--- src/treasury/sendRewards.test.ts
+++ src/treasury/sendRewards.test.ts
@@ -1,133 +1,31 @@
-import NODE_ENDPOINT, {
-  getUser1Wallet,
-  createWallet,
-  getContractsOwnerWallet,
-} from '../util/clients';
-import {
-  customFees,
-  gasPrice,
-  NATIVE_MINIMAL_DENOM,
-  NATIVE_TICKER,
-} from '../util/utils';
+import NODE_ENDPOINT, { getUser1Wallet } from '../util/clients';
+import { customFees, gasPrice, NATIVE_TICKER } from '../util/utils';
 import { NolusClient, NolusContracts, NolusWallet } from '@nolus/nolusjs';
-import { sendInitExecuteFeeTokens } from '../util/transfer';
-import { Asset } from '@nolus/nolusjs/build/contracts';
 import { runOrSkip } from '../util/testingRules';
 
 runOrSkip(process.env.TEST_TREASURY as string)(
   'Treasury tests - Request rewards',
   () => {
     let userWithBalanceWallet: NolusWallet;
-    let contractsOwnerWallet: NolusWallet;
-    let newDispatcherWallet: NolusWallet;
     let treasuryInstance: NolusContracts.Treasury;
     const treasuryContractAddress = process.env.TREASURY_ADDRESS as string;
-    let rewards: Asset;
     let cosm: any;
-
-    const percision = 100000;
-    const gasPriceInteger = gasPrice * percision;
 
     beforeAll(async () => {
       NolusClient.setInstance(NODE_ENDPOINT);
       cosm = await NolusClient.getInstance().getCosmWasmClient();
 
       userWithBalanceWallet = await getUser1Wallet();
-      newDispatcherWallet = await createWallet();
-      contractsOwnerWallet = await getContractsOwnerWallet();
 
       treasuryInstance = new NolusContracts.Treasury(
         cosm,
         treasuryContractAddress,
       );
-
-      rewards = { ticker: NATIVE_TICKER, amount: '100000' };
     });
 
-    test('the configured dispatcher account tries to request rewards from the treasury - should work as expected', async () => {
-      const dispatcherBalanceBeforeFirstReward =
-        await userWithBalanceWallet.getBalance(
-          newDispatcherWallet.address as string,
-          NATIVE_MINIMAL_DENOM,
-        );
+    test('an unregistered dispatcher tries to request rewards from the treasury - should produce an error', async () => {
+      const rewards = { ticker: NATIVE_TICKER, amount: '1000' };
 
-      await sendInitExecuteFeeTokens(
-        userWithBalanceWallet,
-        contractsOwnerWallet.address as string,
-      );
-
-      await treasuryInstance.configRewardsTransfer(
-        contractsOwnerWallet,
-        newDispatcherWallet.address as string,
-        customFees.exec,
-      );
-
-      await sendInitExecuteFeeTokens(
-        userWithBalanceWallet,
-        newDispatcherWallet.address as string,
-      );
-
-      const treasuryBalanceBeforeFirstReward = await cosm.getBalance(
-        treasuryContractAddress,
-        NATIVE_MINIMAL_DENOM,
-      );
-
-      await treasuryInstance.sendRewards(
-        newDispatcherWallet,
-        rewards,
-        customFees.exec,
-      );
-
-      const treasuryBalanceAfterFirstReward = await cosm.getBalance(
-        treasuryContractAddress,
-        NATIVE_MINIMAL_DENOM,
-      );
-
-      const dispatcherBalanceAfterFirstReward =
-        await userWithBalanceWallet.getBalance(
-          newDispatcherWallet.address as string,
-          NATIVE_MINIMAL_DENOM,
-        );
-
-      expect(BigInt(dispatcherBalanceAfterFirstReward.amount)).toBe(
-        BigInt(dispatcherBalanceBeforeFirstReward.amount) +
-          BigInt(rewards.amount),
-      );
-
-      // balanceBefore - rewards + 40%gas
-      expect(BigInt(treasuryBalanceAfterFirstReward.amount)).toBe(
-        BigInt(treasuryBalanceBeforeFirstReward.amount) -
-          BigInt(rewards.amount) +
-          BigInt(customFees.exec.amount[0].amount) -
-          (BigInt(customFees.exec.gas) * BigInt(gasPriceInteger)) /
-            BigInt(percision),
-      );
-
-      // send rewards more than once
-      await sendInitExecuteFeeTokens(
-        userWithBalanceWallet,
-        newDispatcherWallet.address as string,
-      );
-
-      await treasuryInstance.sendRewards(
-        newDispatcherWallet,
-        rewards,
-        customFees.exec,
-      );
-
-      const dispatcherBalanceAfterSecondReward =
-        await userWithBalanceWallet.getBalance(
-          newDispatcherWallet.address as string,
-          NATIVE_MINIMAL_DENOM,
-        );
-
-      expect(BigInt(dispatcherBalanceAfterSecondReward.amount)).toBe(
-        BigInt(dispatcherBalanceAfterFirstReward.amount) +
-          BigInt(rewards.amount),
-      );
-    });
-
-    test('an unauthorized user tries to request rewards from the treasury - should produce an error', async () => {
       const broadcastTx = () =>
         treasuryInstance.sendRewards(
           userWithBalanceWallet,
@@ -135,88 +33,22 @@ runOrSkip(process.env.TEST_TREASURY as string)(
           customFees.exec,
         );
 
-      await expect(broadcastTx).rejects.toThrow(/^.*Unauthorized.*/);
+      await expect(broadcastTx).rejects.toThrow(/^.*Addr not found.*/);
     });
 
     test('an unauthorized user tries to change dispatcher address - should produce an error', async () => {
+      const dispatcherContractAddress = process.env
+        .DISPATCHER_ADDRESS as string;
+
       const broadcastTx = () =>
         treasuryInstance.configRewardsTransfer(
           userWithBalanceWallet,
-          userWithBalanceWallet.address as string as string,
+          dispatcherContractAddress,
           customFees.exec,
         );
 
-      await expect(broadcastTx).rejects.toThrow(/^.*Unauthorized.*/);
-    });
-
-    test('the configured dispatcher account tries to request 0 rewards from the treasury - should produce an error', async () => {
-      await sendInitExecuteFeeTokens(
-        userWithBalanceWallet,
-        newDispatcherWallet.address as string,
-      );
-
-      rewards.amount = '0';
-
-      const broadcastTx = () =>
-        treasuryInstance.sendRewards(
-          newDispatcherWallet,
-          rewards,
-          customFees.exec,
-        );
-
-      await expect(broadcastTx).rejects.toThrow(/^.*0unls: invalid coins.*/);
-    });
-
-    test('the configured dispatcher account tries to request more rewards than the treasury has - should produce an error', async () => {
-      const dispatcherBalanceBefore = await userWithBalanceWallet.getBalance(
-        newDispatcherWallet.address as string,
-        NATIVE_MINIMAL_DENOM,
-      );
-
-      const treasuryBalanceBefore = await cosm.getBalance(
-        treasuryContractAddress,
-        NATIVE_MINIMAL_DENOM,
-      );
-
-      const excess = 1;
-
-      rewards.amount = (
-        BigInt(treasuryBalanceBefore.amount) +
-        BigInt(excess) +
-        BigInt(customFees.exec.amount[0].amount) -
-        (BigInt(customFees.exec.gas) * BigInt(gasPriceInteger)) /
-          BigInt(percision)
-      ).toString();
-
-      const broadcastTx = () =>
-        treasuryInstance.sendRewards(
-          newDispatcherWallet,
-          rewards,
-          customFees.exec,
-        );
-
-      await expect(broadcastTx).rejects.toThrow(/^.*insufficient funds.*/);
-
-      const dispatcherBalanceAfter = await userWithBalanceWallet.getBalance(
-        newDispatcherWallet.address as string,
-        NATIVE_MINIMAL_DENOM,
-      );
-
-      const treasuryBalanceAfter = await cosm.getBalance(
-        treasuryContractAddress,
-        NATIVE_MINIMAL_DENOM,
-      );
-
-      expect(BigInt(treasuryBalanceAfter.amount)).toBe(
-        BigInt(treasuryBalanceBefore.amount) +
-          BigInt(customFees.exec.amount[0].amount) -
-          (BigInt(customFees.exec.gas) * BigInt(gasPriceInteger)) /
-            BigInt(percision),
-      );
-
-      expect(BigInt(dispatcherBalanceAfter.amount)).toBe(
-        BigInt(dispatcherBalanceBefore.amount) -
-          BigInt(customFees.exec.amount[0].amount),
+      await expect(broadcastTx).rejects.toThrow(
+        /^.*Checked address doesn't match the one associated with access control variable.*/,
       );
     });
   },
