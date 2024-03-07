49cb844d908a21b29874583877f07ea18f796272
desislavva <dessislv28th@gmail.com>
2024-03-07 16:26:08 +0200

    feat: Lender - test permissions

---
+++ src/lender/permissions.test.ts
@@ -0,0 +1,31 @@
+import { NolusClient, NolusWallet } from '@nolus/nolusjs';
+import { customFees } from '../util/utils';
+import NODE_ENDPOINT, { getUser1Wallet } from '../util/clients';
+import { runOrSkip } from '../util/testingRules';
+
+runOrSkip(process.env.TEST_LENDER as string)(
+  'Lender tests - Permissions',
+  () => {
+    let userWithBalanceWallet: NolusWallet;
+    const lppContractAddress = process.env.LPP_ADDRESS as string;
+
+    beforeAll(async () => {
+      NolusClient.setInstance(NODE_ENDPOINT);
+      userWithBalanceWallet = await getUser1Wallet();
+    });
+
+    test('lease code id should only be updated by the Leaser contract', async () => {
+      const newLeaseCodeMsg = { new_lease_code: { lease_code_id: '2' } };
+
+      const repayLoan = () =>
+        userWithBalanceWallet.execute(
+          userWithBalanceWallet.address as string,
+          lppContractAddress,
+          newLeaseCodeMsg,
+          customFees.exec,
+        );
+
+      await expect(repayLoan).rejects.toThrow(/^.*Unauthorized access.*/);
+    });
+  },
+);
