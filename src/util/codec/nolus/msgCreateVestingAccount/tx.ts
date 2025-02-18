2704170e108efcda124f939b112b65f55d870bf0
desislavva <dessislv28th@gmail.com>
2025-02-18 14:51:46 +0200

    fix: codec

--- src/util/codec/nolus/msgCreateVestingAccount/tx.ts
+++ src/util/codec/nolus/msgCreateVestingAccount/tx.ts
@@ -236,7 +236,8 @@ interface Rpc {
 type Builtin = Date | Function | Uint8Array | string | number | boolean | undefined;
 
 export type DeepPartial<T> = T extends Builtin ? T
-  : T extends Long ? string | number | Long : T extends Array<infer U> ? Array<DeepPartial<U>>
+  : T extends number | string | bigint ? T
+  : T extends Array<infer U> ? Array<DeepPartial<U>>
   : T extends ReadonlyArray<infer U> ? ReadonlyArray<DeepPartial<U>>
   : T extends {} ? { [K in keyof T]?: DeepPartial<T[K]> }
   : Partial<T>;
